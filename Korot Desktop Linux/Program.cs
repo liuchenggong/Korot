6bfae42a7d8bb3b7fb1c6410f4cd680a15a16609
haltroy <thehaltroy@gmail.com>
2020-10-12 16:24:51 +0300

    Work on Linux

--- Korot Desktop Linux/Program.cs
+++ Korot Desktop Linux/Program.cs
@@ -1,24 +1,30 @@
 ﻿using System;
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Logging.Serilog;
-using Avalonia.ReactiveUI;
 
-namespace Korot_Desktop_Linux
+namespace Korot
 {
-    class Program
-    {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+	class Program
+	{
+		// Initialization code. Don't use any Avalonia, third-party APIs or any
+		// SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+		// yet and stuff might break.
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			BuildKorot()
+			// workaround for https://github.com/AvaloniaUI/Avalonia/issues/3533
+			.With(new AvaloniaNativePlatformOptions { UseGpu = false })
+			.StartWithClassicDesktopLifetime(args);
+		}
 
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .LogToDebug()
-                .UseReactiveUI();
-    }
+
+		// Avalonia configuration, don't remove; also used by visual designer.
+		public static AppBuilder BuildKorot()
+			=> AppBuilder.Configure<App>()
+				.UsePlatformDetect()
+				.LogToDebug();
+	}
 }
